0dafe808a70a1ec13cbd147ef370f77699345346
Hardntrash <hardntrash@gmail.com>
2018-09-06 22:29:47 +0500

    Подготовка к деплою

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 from FlaskApp import app
 
 app.config['CSRF_ENABLED'] = True
-app.config['SECRET_KEY'] = 'fuck-u'
+app.config['SECRET_KEY'] = 'iuhiuh3iu1h2o3h1oui2h3981h23921u098123i12'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
