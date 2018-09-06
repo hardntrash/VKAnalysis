95d44f4b81dd1a9d7976ab4e6123553201c56374
Fractight <goglbummm@gmail.com>
2018-09-06 22:19:24 +0500

    New buttons for methods

--- FlaskApp/forms.py
+++ FlaskApp/forms.py
@@ -3,9 +3,29 @@ from wtforms import TextField, SubmitField
 from wtforms.validators import Required
 
 class IndexForm(FlaskForm):
-    vk_id = TextField('ID', validators=[Required()])
-    submit = SubmitField('Go')
+    vk_id = TextField(
+    	'ID',
+    	validators=[Required()],
+    	render_kw={
+    		"placeholder": "Введи id",
+    		"class": "input-line__input",
+    	})
+    submit = SubmitField(
+    	'Go',
+    	render_kw = {
+    		"class": "input-line__submit",
+    	})
 
 class MapLikesForms(FlaskForm):
-    vk_id = TextField('ID', validators=[Required()])
-    submit = SubmitField('Go Map')
+    vk_id = TextField(
+       	'ID',
+    	validators = [Required()],
+    	render_kw = {
+    		"placeholder": "Введи id группы или человека",
+    		"class": "input-line__input",
+    	})
+    submit = SubmitField(
+    	'Go Map',
+    	render_kw = {
+    		"class": "input-line__submit",
+    	})
